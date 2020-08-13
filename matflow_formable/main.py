29f67ab45130fdec3297a31e5e3b44fca8bae4e4
Elliot Cooksey-Nash <ecookseynash@gmail.com>
2020-08-13 15:13:59 +0100

    The output for fit_yield_function has been changed to native python format

--- matflow_formable/main.py
+++ matflow_formable/main.py
@@ -11,7 +11,7 @@ from matflow_formable import (
 
 from formable import __version__ as formable_version
 from formable.load_response import LoadResponse, LoadResponseSet
-    
+from formable.maths_utils import get_principal_values   
     
 
 
@@ -36,9 +36,36 @@ def fit_yield_function(yield_function_name, yield_point_criteria, uniaxial_respo
     response_set.calculate_yield_stresses(yield_point_criteria)
     response_set.fit_yield_function(yield_function_name, uniaxial_response=uni_resp)
     
-    print('outputs: .yield_functions, .yield_stresses, .show_yield_functions_2D(), .show_yield_functions_3D()')
+    # dictionary of the stress tensors for each deformation
+    stress_tensors = {}
+    num = range(len(multi_resp))
+    for n in num:
+        stress_tensors['deformation_{}'.format(n+1)] = multi_resp[n].incremental_data['true_stress']
     
-    return response_set
+    # dictionary of eqivalent strain values for each deformation
+    equivalent_plastic_strain = {}
+    num = range(len(multi_resp))
+    for n in num:
+        equivalent_plastic_strain['deformation_{}'.format(n)] = multi_resp[n].incremental_data['equivalent_plastic_strain']
+    
+    
+    
+    out = {
+        
+        'fitted_yield_functions': [ {'name': yield_function_name, **i['yield_function'].get_parameters() }for i in response_set.yield_functions],
+  
+        'yield_stress_tensors':response_set.yield_stresses[0]['values'],
+        
+        'yield_stress_eigenvalues_xyz' : get_principal_values(response_set.yield_stresses[0]['values']),
+        
+        'stress_tensors':stress_tensors,
+        
+        'equivalent_plastic_strain':equivalent_plastic_strain
+
+       }
+    
+    
+    return out
 
 
 @software_versions()
