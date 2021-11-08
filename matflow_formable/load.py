98cc94348521fc7d27d1a7a369c6532440a46988
APlowman <adam.jp@live.co.uk>
2021-11-08 19:52:16 +0000

    Refactor to allow for mixed load cases

--- matflow_formable/load.py
+++ matflow_formable/load.py
@@ -5,13 +5,18 @@ from matflow.scripting import get_wrapper_script
 
 from matflow_formable import input_mapper, output_mapper, sources_mapper
 
+@sources_mapper(task='generate_load_case', method='uniaxial', script='build_load_case')
+@sources_mapper(task='generate_load_case', method='biaxial', script='build_load_case')
+@sources_mapper(task='generate_load_case', method='plane_strain', script='build_load_case')
+@sources_mapper(task='generate_load_case', method='planar_2D', script='build_load_case')
+@sources_mapper(task='generate_load_case', method='random_2D', script='build_load_case')
+@sources_mapper(task='generate_load_case', method='random_3D', script='build_load_case')
+@sources_mapper(task='generate_load_case', method='cyclic_uniaxial', script='build_load_case')
+@sources_mapper(task='generate_load_case', method='mixed', script='build_load_case')
+def build_load_case():
 
-@sources_mapper(task='generate_load_case', method='uniaxial',
-                script='get_load_case_uniaxial')
-def get_load_case_uniaxial():
-
-    script_name = 'get_load_case_uniaxial.py'
-    snippets = [{'name': 'get_load_case_uniaxial.py'}]
+    script_name = 'build_load_case.py'
+    snippets = [{'name': 'build_load_case.py'}]
     outputs = ['load_case']
     out = {
         'script': {
@@ -20,115 +25,26 @@ def get_load_case_uniaxial():
         }
     }
     return out
-
-@sources_mapper(task='generate_load_case', method='cyclic_uniaxial',
-                script='get_load_case_uniaxial_cyclic')
-def get_load_case_uniaxial_cyclic():
-
-    script_name = 'get_load_case_uniaxial_cyclic.py'
-    snippets = [{'name': 'get_load_case_uniaxial_cyclic.py'}]
-    outputs = ['load_case']
-    out = {
-        'script': {
-            'content': get_wrapper_script(__package__, script_name, snippets, outputs),
-            'filename': script_name,
-        }
-    }
-    return out
-
-@sources_mapper(task='generate_load_case', method='biaxial',
-                script='get_load_case_biaxial')
-def get_load_case_biaxial():
-
-    script_name = 'get_load_case_biaxial.py'
-    snippets = [{'name': 'get_load_case_biaxial.py'}]
-    outputs = ['load_case']
-    out = {
-        'script': {
-            'content': get_wrapper_script(__package__, script_name, snippets, outputs),
-            'filename': script_name,
-        }
-    }
-    return out
-
-
-@sources_mapper(task='generate_load_case', method='plane_strain',
-                script='get_load_case_plane_strain')
-def get_load_case_plane_strain():
-
-    script_name = 'get_load_case_plane_strain.py'
-    snippets = [{'name': 'get_load_case_plane_strain.py'}]
-    outputs = ['load_case']
-    out = {
-        'script': {
-            'content': get_wrapper_script(__package__, script_name, snippets, outputs),
-            'filename': script_name,
-        }
-    }
-    return out
-
-@sources_mapper(task='generate_load_case', method='planar_2D',
-                script='get_load_case_planar_2D')
-def get_load_case_planar_2D():
-
-    script_name = 'get_load_case_planar_2D.py'
-    snippets = [{'name': 'get_load_case_planar_2D.py'}]
-    outputs = ['load_case']
-    out = {
-        'script': {
-            'content': get_wrapper_script(__package__, script_name, snippets, outputs),
-            'filename': script_name,
-        }
-    }
-    return out
-
-
-@sources_mapper(task='generate_load_case', method='random_2D',
-                script='get_load_case_random_2D')
-def get_load_case_random_2D():
-
-    script_name = 'get_load_case_random_2D.py'
-    snippets = [{'name': 'get_load_case_random_2D.py'}]
-    outputs = ['load_case']
-    out = {
-        'script': {
-            'content': get_wrapper_script(__package__, script_name, snippets, outputs),
-            'filename': script_name,
-        }
-    }
-    return out
-
-
-@sources_mapper(task='generate_load_case', method='random_3D',
-                script='get_load_case_random_3D')
-def get_load_case_random_3D():
-
-    script_name = 'get_load_case_random_3D.py'
-    snippets = [{'name': 'get_load_case_random_3D.py'}]
-    outputs = ['load_case']
-    out = {
-        'script': {
-            'content': get_wrapper_script(__package__, script_name, snippets, outputs),
-            'filename': script_name,
-        }
-    }
-    return out
-
 
 @input_mapper(input_file='inputs.hdf5', task='generate_load_case', method='uniaxial')
 def write_load_case_uniaxial_param_file(path, total_times, num_increments, directions,
                                         target_strain_rates, target_strains,
                                         rotations, dump_frequency):
-    kwargs = {
-        'total_times': total_times,
-        'num_increments': num_increments,
-        'directions': directions,
-        'target_strain_rates': target_strain_rates,
-        'target_strains': target_strains,
-        'rotations': rotations,
-        'dump_frequency': dump_frequency,
+    out = {
+        'load_case_specs': [
+            {
+                'type': 'uniaxial',
+                'total_times': total_times,
+                'num_increments': num_increments,
+                'directions': directions,
+                'target_strain_rates': target_strain_rates,
+                'target_strains': target_strains,
+                'rotations': rotations,
+                'dump_frequency': dump_frequency,
+            }
+        ]
     }
-    hickle.dump(kwargs, path)
+    hickle.dump(out, path)
 
 
 @input_mapper(input_file='inputs.hdf5', task='generate_load_case', method='cyclic_uniaxial')
@@ -136,82 +52,112 @@ def write_load_case_uniaxial_cyclic_param_file(path, max_stresses, min_stresses,
                                                cycle_frequencies, num_increments_per_cycle,
                                                num_cycles, directions, waveforms,
                                                dump_frequency):
-    kwargs = {
-        'max_stresses': max_stresses,
-        'min_stresses': min_stresses,
-        'cycle_frequencies': cycle_frequencies,
-        'num_increments_per_cycle': num_increments_per_cycle,
-        'num_cycles': num_cycles,
-        'directions': directions,
-        'waveforms': waveforms,
-        'dump_frequency': dump_frequency,
-    }
-    hickle.dump(kwargs, path)
+    out = {
+        'load_case_specs': [
+            {
+                'type': 'cyclic_uniaxial',
+                'max_stresses': max_stresses,
+                'min_stresses': min_stresses,
+                'cycle_frequencies': cycle_frequencies,
+                'num_increments_per_cycle': num_increments_per_cycle,
+                'num_cycles': num_cycles,
+                'directions': directions,
+                'waveforms': waveforms,
+                'dump_frequency': dump_frequency,
+            }
+        ]
+    }    
+    hickle.dump(out, path)
+
+
+@input_mapper(input_file='inputs.hdf5', task='generate_load_case', method='mixed')
+def write_load_case_mixed_param_file(path, load_case_specs):
+    hickle.dump({'load_case_specs': load_case_specs}, path)
 
 
 @input_mapper(input_file='inputs.hdf5', task='generate_load_case', method='biaxial')
 def write_load_case_biaxial_param_file(path, total_times, num_increments, directions,
                                        target_strain_rates, target_strains,
                                        rotations, dump_frequency):
-    kwargs = {
-        'total_times': total_times,
-        'num_increments': num_increments,
-        'directions': directions,
-        'target_strain_rates': target_strain_rates,
-        'target_strains': target_strains,
-        'rotations': rotations,
-        'dump_frequency': dump_frequency,
+    out = {
+        'load_case_specs': [
+            {
+                'type': 'biaxial',
+                'total_times': total_times,
+                'num_increments': num_increments,
+                'directions': directions,
+                'target_strain_rates': target_strain_rates,
+                'target_strains': target_strains,
+                'rotations': rotations,
+                'dump_frequency': dump_frequency,
+            }
+        ]
     }
-    hickle.dump(kwargs, path)
+    hickle.dump(out, path)
 
 
 @input_mapper(input_file='inputs.hdf5', task='generate_load_case', method='plane_strain')
 def write_load_case_plane_strain_param_file(path, total_times, num_increments, directions,
                                             target_strain_rates, target_strains,
                                             dump_frequency, rotations, strain_rate_modes):
-    kwargs = {
-        'total_times': total_times,
-        'num_increments': num_increments,
-        'directions': directions,
-        'target_strain_rates': target_strain_rates,
-        'target_strains': target_strains,
-        'rotations': rotations,
-        'strain_rate_modes': strain_rate_modes,
-        'dump_frequency': dump_frequency,
+    out = {
+        'load_case_specs': [
+            {
+                'type': 'plane_strain',
+                'total_times': total_times,
+                'num_increments': num_increments,
+                'directions': directions,
+                'target_strain_rates': target_strain_rates,
+                'target_strains': target_strains,
+                'rotations': rotations,
+                'strain_rate_modes': strain_rate_modes,
+                'dump_frequency': dump_frequency,
+            }
+        ]
     }
-    hickle.dump(kwargs, path)
+    hickle.dump(out, path)
 
 
 @input_mapper(input_file='inputs.hdf5', task='generate_load_case', method='planar_2D')
 def write_load_case_planar_2D_param_file(path, total_times, num_increments,
                                          normal_directions, target_strain_rates,
                                          target_strains, rotations, dump_frequency):
-    kwargs = {
-        'total_times': total_times,
-        'num_increments': num_increments,
-        'normal_directions': normal_directions,
-        'target_strain_rates': target_strain_rates,
-        'target_strains': target_strains,
-        'rotations': rotations,
-        'dump_frequency': dump_frequency,
+    out = {
+        'load_case_specs': [
+            {
+                'type': 'planar_2D',
+                'total_times': total_times,
+                'num_increments': num_increments,
+                'normal_directions': normal_directions,
+                'target_strain_rates': target_strain_rates,
+                'target_strains': target_strains,
+                'rotations': rotations,
+                'dump_frequency': dump_frequency,
+            }
+        ]
     }
-    hickle.dump(kwargs, path)
+    hickle.dump(out, path)
 
 
 @input_mapper(input_file='inputs.hdf5', task='generate_load_case', method='random_2D')
 def write_load_case_random_2D_param_file(path, total_times, num_increments,
                                          normal_directions, target_strain_rates,
                                          target_strains, rotations, dump_frequency):
-    kwargs = {
-        'total_times': total_times,
-        'num_increments': num_increments,
-        'normal_directions': normal_directions,
-        'target_strain_rates': target_strain_rates,
-        'target_strains': target_strains,
-        'rotations': rotations,
-        'dump_frequency': dump_frequency,
-    }
-    hickle.dump(kwargs, path)
+    out = {
+        'load_case_specs': [
+            {
+                'type': 'random_2D',
+                'total_times': total_times,
+                'num_increments': num_increments,
+                'normal_directions': normal_directions,
+                'target_strain_rates': target_strain_rates,
+                'target_strains': target_strains,
+                'rotations': rotations,
+                'dump_frequency': dump_frequency,
+            }
+        ]
+    }    
+    hickle.dump(out, path)
 
 
 @input_mapper(input_file='inputs.hdf5', task='generate_load_case', method='random_3D')
@@ -219,25 +165,31 @@ def write_load_case_random_3D_param_file(path, total_times, num_increments,
                                          target_strains, rotation, rotation_max_angle,
                                          rotation_load_case, non_random_rotation,
                                          dump_frequency):
-    kwargs = {
-        'total_times': total_times,
-        'num_increments': num_increments,
-        'target_strains': target_strains,
-        'rotation': rotation,
-        'rotation_max_angle': rotation_max_angle,
-        'rotation_load_case': rotation_load_case,
-        'non_random_rotation': non_random_rotation,
-        'dump_frequency': dump_frequency,
+    out = {
+        'load_case_specs': [
+            {
+                'type': 'random_3D',
+                'total_times': total_times,
+                'num_increments': num_increments,
+                'target_strains': target_strains,
+                'rotation': rotation,
+                'rotation_max_angle': rotation_max_angle,
+                'rotation_load_case': rotation_load_case,
+                'non_random_rotation': non_random_rotation,
+                'dump_frequency': dump_frequency,
+            }
+        ]
     }
-    hickle.dump(kwargs, path)
+    hickle.dump(out, path)
 
 
 @output_mapper(output_name='load_case', task='generate_load_case', method='uniaxial')
-@output_mapper(output_name='load_case', task='generate_load_case', method='cyclic_uniaxial')
 @output_mapper(output_name='load_case', task='generate_load_case', method='biaxial')
 @output_mapper(output_name='load_case', task='generate_load_case', method='plane_strain')
 @output_mapper(output_name='load_case', task='generate_load_case', method='planar_2D')
 @output_mapper(output_name='load_case', task='generate_load_case', method='random_2D')
 @output_mapper(output_name='load_case', task='generate_load_case', method='random_3D')
+@output_mapper(output_name='load_case', task='generate_load_case', method='cyclic_uniaxial')
+@output_mapper(output_name='load_case', task='generate_load_case', method='mixed')
 def read_load_case(path):
     return hickle.load(path)
